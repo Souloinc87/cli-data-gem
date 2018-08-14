f52aa59cc18bb700e9761774c3683c9586dc720e
Marcus Edwards 111 <Soulo@Marcuss-MacBook-Pro.local>
2018-08-14 17:42:46 -0500

    second commit

--- lib/perfect/models/cli.rb
+++ lib/perfect/models/cli.rb
@@ -5,7 +5,7 @@ class CLI
   attr_accessor :charity
 
     def start
-    
+
       puts " "
       puts "Welcome to Perfect Charity"
       puts " "
@@ -39,22 +39,33 @@ class CLI
         if input.to_i == 1
             puts " "
             puts Charity.scrape('https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=12551')
+            puts " "
+            puts "Donate here:  https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=12551"
             user_input
+
         elsif input.to_i == 2
             puts " "
             puts Charity.scrape('https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=8609')
+            puts " "
+            puts "Donate here:  https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=8609"
             user_input
         elsif input.to_i == 3
             puts " "
             puts Charity.scrape('https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=13885')
+            puts " "
+            puts "Donate here:   https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=13885"
             user_input
         elsif input.to_i == 4
             puts " "
             puts Charity.scrape('https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=14793')
+            puts " "
+            puts "Donate here:   https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=14793"
             user_input
         elsif input.to_i == 5
             puts " "
             puts Charity.scrape('https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=12756')
+            puts " "
+            puts "Donate here:   https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=12756"
             user_input
         elsif input.to_i == 6
             start
